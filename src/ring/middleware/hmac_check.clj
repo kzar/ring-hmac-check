e85f06e5c41a0bdec73ea190e0f82474d1321f00
David Barker <kzar@kzar.co.uk>
2011-10-06 11:19:06 +0100

    Made some changes to the hmac check wrapping function.

--- src/ring/middleware/hmac_check.clj
+++ src/ring/middleware/hmac_check.clj
@@ -12,17 +12,24 @@
        (.doFinal mac (.getBytes msg encoding)))))
 
 (defn wrap-hmac-check
-  [handler {:keys [algorithm header-field secret-key forbidden-handler pred]
+  "Function used to add the hmac-check middleware to the Ring stack. By default this will
+  check POST requests for a Hex encoded digest and if wrong overwrite the response as 403 forbidden.
+    - algorithm should be a algorithm string, for example HmacSHA512
+    - header-field should be the key for the hmac in the header
+    - forbidden-handler, digest-decoder and pred are functions that can be overwritten to change
+      default behavoir"
+  [handler {:keys [algorithm header-field secret-key forbidden-handler digest-decoder pred]
             :or [forbidden-handler (fn [req]
                                      {:status 403 :body "403 Forbidden - Incorrect HMAC"})
-                 pred (fn [req] (= :post (:request-method req)))]}]
+                 pred (fn [req] (= :post (:request-method req)))
+                 digest-decoder (fn [digest] (-> digest char-array Hex/decodeHex))]}]
   {:pre [(every? identity [algorithm header-field secret-key])]}
   (fn [req]
     (handler
      (if (pred req)
-       (let [given-hmac (Base64/decodeBase64 (header-field (:header req)))
+       (let [given-hmac (digest-decoder (header-field (:headers req)))
              our-hmac (hmac algorithm (:body req) secret-key)]
-         (if (Arrays/equals given-hmac our-hmac)
+         (if (Arrays/equals (digest-decoder given-hmac) our-hmac)
            req
            (forbidden-handler req)))
        req))))
--- test/ring/middleware/hmac_check_test.clj
+++ test/ring/middleware/hmac_check_test.clj
@@ -1,17 +1,24 @@
 (ns ring.middleware.hmac-check-test
-  (:import [org.apache.commons.codec.binary Base64]
+  (:import [org.apache.commons.codec.binary Base64 Hex]
            [java.util Arrays])
   (:use clojure.test
         ring.middleware.hmac-check))
 
 ; Test the HMAC function works
 (deftest hmac-sha512
-  (is (Arrays/equals (hmac "HmacSHA512" "hello world" "test")
-         (Base64/decodeBase64 "MJ7MSJwS1utMxA9QyQLytNDtd+5RGnx6m808qG1M2G+YndNbxf9JlnDaNCVbRbDP2DDoH2Bdz33FVC6TrpzXbw=="))))
+  (is (Arrays/equals (hmac "HmacSHA512" "test" "hello world")
+                     (-> "f39526a1625c5ef672f250037d3b9669e2c6d38c8e19c30344ff04a4fb048ea556befd34ce6dc8fbc7667c76e33c6053bf603ab5760b6e55ce9ab5f1d2274035" char-array Hex/decodeHex))))
 
 (def hmac-check-handler (wrap-hmac-check identity {:algorithm "HmacSHA512" :header-field "AUTH-HMAC"
                                                    :secret-key "very-secret-key"}))
 
-; Test that valid requets are left alone
-;(deftest valid-request
-;  (hmac-check-handler 
+; Test that valid requests are left alone
+(deftest valid-request
+  (let [request {:uri "/" :request-method :post
+                 :server-port 80
+                 :server-name "dave.inadub.co.uk"
+                 :remote-addr "localhost"
+                 :scheme :http
+                 :headers {"AUTH-HMAC" "dcb14db632d96a3a4dd9259c858242b037b3e95bd569744097231aa0c042ff147b927d0a4704e82732a250717851e89e64421793f259185c0675a64694966da3"}
+                 :body "This is the test body"}]
+    (is (= (hmac-check-handler request) request))))
