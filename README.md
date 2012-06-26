2e0cc19c878d1de1cf21af970630b11e392532c8
David Barker <kzar@kzar.co.uk>
2012-06-26 11:14:34 +0100

    Upped version and updated README.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 [Ring](http://github.com/mmcgrana/ring) middleware that checks POST requests have a valid HMAC of the HTTP POST body in the header.
 
-In development, feedback welcome!
-
 ### Usage
 
 #### With Noir - in server.clj
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-hmac-check "0.1.0"
+(defproject ring-hmac-check "0.1.1"
   :description "Ring middleware for HMAC checking."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec/commons-codec "1.4"]]
