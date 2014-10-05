58918feb526520319513f96a3d1650879f424b23
Dave Barker <kzar@kzar.co.uk>
2014-10-05 15:16:09 +0100

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject ring-hmac-check "0.2.0"
+(defproject ring-hmac-check "0.2.1"
   :description "Ring middleware for HMAC checking."
+  :url "https://github.com/kzar/ring-hmac-check"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec/commons-codec "1.4"]]
   :profiles {
